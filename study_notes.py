fc19e457f46a3ffd648ce2a3540c1da66849e95f
andressa-leite <dre_tl@hotmail.com>
2026-01-21 12:46:48 -0300

    Import, analyze, and prepare the data

---
+++ study_notes.py
@@ -0,0 +1,4 @@
+#o pandas é utilizado para importar e manipular os dados e ele fica instalado no computador através do comando pip install pandas, então não é necessário instalar toda vez que for utilizar
+#o pd é um apelido padrão para o pandas, utilizado para facilitar a escrita do código
+#o display(tabela) é utilizado para exibir a tabela de forma mais legível
+#o display(tabela.info()) é utilizado para exibir informações sobre a tabela, como tipos de dados e valores nulos. É importante tratar valores nulos antes de utilizar os dados para treinar um modelo de Machine Learning ou fazer análise dos dados.
